a913fc99d7be29295758d528adf39e075e85cd26
matthieu.laniesse@gmail.com <bevada09>
2018-01-08 16:30:30 +0100

    footer

---
+++ web/test.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+ </head>
+ <body>
+
+<div id="main">
+
+<footer>
+
+ <?php include "footer.html"; ?>
+
+</footer>
+
+</div>
+
+</body>
+</html>
