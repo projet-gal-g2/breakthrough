946c2d7c3bc8083855503e578f5e6531f49a407e
Benjamin Cléton <benjamin.cleton@hotmail.com>
2018-01-08 16:26:59 +0100

    modif

---
+++ src/web/Plateau.js
@@ -0,0 +1,21 @@
+/**
+ * Created by bcleton on 08/01/18.
+ */
+Breakthrough.Plateau = function(c) {
+        var container = c;
+        var board;
+
+        this.initialize = function() {
+            board = new Array();
+            for(var i = 0; i < 8; i++)
+            {
+                board[i] = new Array();
+                for(var j = 0; j < 8; j++)
+                {
+
+                    board[i][j] =
+                }
+            }
+        };
+
+};
