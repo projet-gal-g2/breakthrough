60635537d80eba664316cdb8c78a8546337b4ac7
Benjamin Cléton <benjamin.cleton@hotmail.com>
2018-01-09 11:09:44 +0100

    modif

--- src/Javascript/Plateau.js
+++ src/Javascript/Plateau.js
@@ -9,7 +9,31 @@ Breakthrough.Plateau = function(c) {
 
         this.initialize = function() {
 
-            container.write("<table border=0 cellspacing=0 cellpadding=0 width="+(square_dim*8+8))
+            container.write("<table border=0 cellspacing=0 cellpadding=0 width="+(square_dim*8+8) + ">");
+            var color = 0;
+            for(var i = 0; i < 8; i++)
+            {
+                container.write("<tr>");
+                for(var j = 0; i < 8; i++)
+                {
+                    switch(color)
+                    {
+                        case 0:
+                            container.write("<td><div style='background-color: black' width=" + square_dim + " height=" + square_dim + "</div></td>");
+                            break;
+
+                        case 1:
+                            container.write("<td><div style='background-color: white' width=" + square_dim + " height=" + square_dim + "</div></td>");
+                            break;
+                    }
+
+                    color = (color + 1) % 2;
+                }
+
+                container.write("</tr>");
+            }
+
+            container.write("</table>");
         };
 
 };
