7d06cff017b98ca51dcd8d852383d64ed0d0d403
lducoroy <l.ducoroy@gmail.com>
2018-01-09 11:24:09 +0100

    Modification des tests de l'initialisation. Ajout des tests sur les get et set pour récupérer un pion

--- src/Javascript/engineTest/engineTest.js
+++ src/Javascript/engineTest/engineTest.js
@@ -26,19 +26,23 @@ BreakthroughTestCase.prototype.testTwoFirstLines = function () {
 BreakthroughTestCase.prototype.testMiddleBoard = function () {
     var midBoard = [];
     var newEngine = new Breakthrough.Engine();
+    newEngine.initialisation();
     var initBoard = newEngine.getGameBoard();
     var errorCounter = 0;
     var lastTwoLines = Breakthrough.SIZEBOARD - (Breakthrough.ONE_LINE*2);
 
-    for ( var iter = 0; iter < Breakthrough.ONE_LINE*2; iter++) {
+    for ( var iter = 0; iter < Breakthrough.SIZEBOARD/2; iter++) {
         midBoard[iter] = Breakthrough.Piece.EMPTY;
     }
 
     for ( var firstSquares = Breakthrough.ONE_LINE*2; firstSquares <lastTwoLines; firstSquares++ ) {
-        if ( midBoard[firstSquares] !== initBoard[firstSquares] ) {
+
+        if ( midBoard[firstSquares-16] !== initBoard[firstSquares] ) {
+
             errorCounter ++;
         }
     }
+
     assertTrue( errorCounter === 0 );
 };
 
@@ -46,17 +50,61 @@ BreakthroughTestCase.prototype.testMiddleBoard = function () {
 BreakthroughTestCase.prototype.testTwoLastLines = function () {
     var lastLines = [];
     var newEngine = new Breakthrough.Engine();
+    newEngine.initialisation();
     var initBoard = newEngine.getGameBoard();
     var errorCounter = 0;
     var lastTwoLines = Breakthrough.SIZEBOARD - (Breakthrough.ONE_LINE*2);
 
     for ( var iter = 0; iter < Breakthrough.ONE_LINE*2; iter++) {
-        lastLines[iter] = Breakthrough.Piece.BLACK;
+        lastLines[iter] = Breakthrough.Piece.WHITE;
     }
     for ( var lastSquares = lastTwoLines; lastSquares < Breakthrough.SIZEBOARD; lastSquares++ ) {
-        if ( lastLines[lastSquares] !== initBoard[lastSquares] ) {
+        if ( lastLines[lastSquares-48] !== initBoard[lastSquares] ) {
             errorCounter ++;
         }
     }
     assertTrue( errorCounter === 0 );
+};
+
+// Test du getPiece
+BreakthroughTestCase.prototype.testgetPiece = function () {
+    var newEngine = new Breakthrough.Engine();
+    newEngine.initialisation();
+    var initBoard = newEngine.getGameBoard();
+
+    assertTrue(newEngine.getPiece(30) === Breakthrough.Piece.EMPTY);
+};
+
+// Test du setPiece
+BreakthroughTestCase.prototype.testSetPiece = function () {
+    var newEngine = new Breakthrough.Engine();
+    newEngine.initialisation();
+    var initBoard = newEngine.getGameBoard();
+
+    var temp_Piece = newEngine.getPiece(0);
+    newEngine.setPiece(0, Breakthrough.Piece.EMPTY);
+    assertTrue(temp_Piece !== initBoard[0]);
+};
+
+// Test du getPiece2D
+BreakthroughTestCase.prototype.testgetPiece2D = function () {
+    var newEngine = new Breakthrough.Engine();
+    newEngine.initialisation();
+    var initBoard = newEngine.getGameBoard();
+
+    var piece = newEngine.getPiece(48);
+    var piece2D = newEngine.getPiece2D(6,0);
+    assertTrue(piece === piece2D);
+};
+
+// Test du setPiece2D
+BreakthroughTestCase.prototype.testSetPiece2D = function () {
+    var newEngine = new Breakthrough.Engine();
+    newEngine.initialisation();
+    var initBoard = newEngine.getGameBoard();
+
+    var tempPiece = newEngine.getPiece(5);
+    newEngine.setPiece2D(0,5,Breakthrough.Piece.EMPTY);
+
+    assertTrue(tempPiece !== initBoard[5]);
 };
